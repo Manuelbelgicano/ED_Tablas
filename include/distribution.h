c152c69aa1b8f507ab6d3778398270c7ae7b7464
ManuelBelgicano <manuelgbal1@gmail.com>
2018-11-14 12:41:08 +0100

    Termino la clase distribution.h

--- include/distribution.h
+++ include/distribution.h
@@ -34,7 +34,6 @@ private:
 	size_t k;					///> Numero de modalidades
 	void copy(const distribution &orig);		///> Copia una distribucion en otra
 	void set(const size_t &_k);			///> Redistribuye el tamaño de una distribucion
-	void sort();					///> Ordena una distribucion de menor a mayor
 public:
 	/**
 	 * @brief Constructor primitivo
@@ -50,14 +49,34 @@ public:
 	 * @brief Consulta sobre el numero de modalidades
 	 * @return El numero de modalidades
 	 */
-	size_t get_nmod() {return k};
+	size_t get_nmod() const {return k};
+	/**
+	 * @brief Consulta sobre la poblacion de la muestra
+	 * @return El numero de modalidades
+	 */
+	size_t get_pop() const;
 	/**
 	 * @brief Consulta sobre una modalidad de la distribucion
 	 * @param i Modalidad que se quiere consultar
 	 * @return Un par modalidad-frecuencia
 	 * @pre i debe ser una modalidad valida
 	 */
-	float *get_mod(const size_t &i);
+	float* get_mod(const size_t &i) const;
+	/**
+	 * @brief Calcula las frecuencias relativas de la distribucion
+	 * @return Un vector con las frecuencias relativas
+	 */
+	std::vector<float> get_fi() const;
+	/**
+	 * @brief Calcula las frecuencias absolutas acumuladas de la distribucion
+	 * @return Un vector con las frecuencias absolutas acumuladas
+	 */
+	std::vector<unsigned int> get_Ni() const;
+	/**
+	 * @brief Calcula las frecuencias relativas acumuladas de la distribucion
+	 * @return Un vector con las frecuencias relativas acumuladas
+	 */
+	std::vector<float> get_Fi() const;
 	/**
 	 * @brief Modifica el numero de modalidades de una distribucion
 	 * @param _k Nuevo numero de modalidades
@@ -66,11 +85,29 @@ public:
 	void set_nmod(const size_t &_k);
 	/**
 	 * @brief Aniade una modalidad a la distribucion
+	 * @param i Posicion donde se quiere aniadir la modalidad
 	 * @param x Modalidad a aniadir
 	 * @param n Frecuencia absoluta de la modalidad
 	 * @pre La frecuencia no debe ser vacia
+	 *	i debe ser una modalidad valida
 	 */
-	void add_mod(const float &x,const unsigned int &n);
+	void add_mod(const size_t &i=-1,const float &x,const unsigned int &n);
+	/**
+	 * @brief Elimina una modalidad a la distribucion
+	 * @param i Modalidad a eliminar
+	 * @pre i debe ser una modalidad valida
+	 */
+	void subst_mod(const size_t &i);
+	/**
+	 * @brief Ordena la distribucion de menor a mayor
+	 */
+	void sort();
+	/**
+	 * @brief Sobrecarga del operador de asignacion
+	 * @param dist Distribucion que se asigna
+	 * @return La distribucion con la asignacion realizada
+	 */
+	 distribution& operator=(const distribution &dist);
 };
 
 #endif
--- src/distribution.cpp
+++ src/distribution.cpp
@@ -18,6 +18,84 @@ void distribution::set(const size_t &_k) {
 	ni.resize(_k);
 }
 
+distribution::distribution(size_t _k=0) {
+	if (_k>0)
+		set(_k);
+	else
+		set(0);
+}
+
+distribution::distribution(const distribution &orig) {
+	copy(orig);
+}
+
+size_t distribution::get_pop() const {
+	int aux = 0;
+	for (size_t i=0;i<k;i++)
+		aux += ni[i];
+	return aux;
+}
+
+float* distribution::get_mod(const size_t &i) const{
+	float mod[2];
+	mod[0] = xi[i];
+	mod[1] = ni[i];
+	return mod;
+}
+
+std::vector<float> distribution::get_fi() const {
+	std::vector<float> fi;
+	unsigned int n = get_pop();
+	for (size_t i=0;i<k;i++)
+		fi.push_back((float)ni[i]/n);
+	
+	return fi;
+}
+
+std::vector<float> distribution::get_Fi() const {
+	std::vector<float> fi = get_fi();
+	std::vector<float> Fi;
+	float aux = 0;
+	for (size_t i=0;i<k;i++) {
+		aux += fi[i];
+		Fi.push_back(aux);
+	}
+	
+	return Fi;
+}
+
+std::vector<unsigned int> distribution::get_Ni() const {
+	std::vector<unsigned int> Ni;
+	float aux = 0;
+	for (size_t i=0;i<k;i++) {
+		aux += ni[i];
+		Ni.push_back(aux);
+	}
+	
+	return Ni;
+}
+
+void distribution::set_nmod(const size_t &_k) {
+	set(_k);
+}
+
+void distribution::add_mod(const size_t &i,const float &x,const unsigned int &n) {
+	if (i!=-1) {
+		xi[i] = x;
+		ni[i] = n;
+	} else {
+		xi.push_back(x);
+		ni.push_back(n);
+		k++;
+	}
+}
+
+void subst_mod(const size_t &i) {
+	xi.erase(xi.begin()+i);
+	ni.erase(ni.begin()+i);
+	k--;	
+}
+
 void distribution::sort() {
 	float min;
 	unsigned int auxfrec;
@@ -41,30 +119,7 @@ void distribution::sort() {
 	}
 }
 
-distribution::distribution(size_t _k=0) {
-	if (_k>0)
-		set(_k);
-	else
-		set(0);
-}
-
-distribution::distribution(const distribution &orig) {
-	copy(orig);
-}
-
-float distribution::*get_mod(const size_t &i) {
-	float mod[2];
-	mod[0] = xi[i];
-	mod[1] = ni[i];
-	return mod;
-}
-
-void distribution::set_nmod(const size_t &_k) {
-	set(_k);
-}
-
-void distribution::add_mod(const float &x,const unsigned int &n) {
-	xi.push_back(x);
-	ni.push_back(n);
-	k++;
+distribution& distribution::operator=(const distribution &dist) {
+	copy(dist);
+	return aux;
 }
